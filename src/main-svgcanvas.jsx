0114e975f403a4c4b51a4062ae6707fb31e06582
trech_live <artemalesin89778@gmail.com>
2024-03-20 23:44:40 +0300

    big changes, trying with translate3d

--- src/main-svgcanvas.jsx
+++ src/main-svgcanvas.jsx
@@ -1,6 +1,7 @@
 import React from "react"
 import cf from './main-svgcanvas-funcs.js';
 
+<<<<<<< HEAD
 function MainSVGCanvas(props) {
   return (
     <div id = 'main-svgcanvas' className="soft-angles mpz"
@@ -11,8 +12,18 @@ function MainSVGCanvas(props) {
       onMouseMove={cf.drag}
       onMouseUp={cf.drag_end}
       onWheel={cf.mouse_wheel}>
+=======
+function MainSVGCanvas() {
+  return (
+    <div id = 'main-svgcanvas' className="soft-angles mpz">
+        <cf.get_map_svg></cf.get_map_svg>
+>>>>>>> ed6619d (big changes, trying with translate3d)
     </div>
   );
 }
 
+<<<<<<< HEAD
 export default MainSVGCanvas;
+=======
+export default MainSVGCanvas;
+>>>>>>> ed6619d (big changes, trying with translate3d)
